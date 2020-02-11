f19895f2f85c6d6a9973025925a59eaf3bc8eaa3
Becky Zorn <bczorn@gmail.com>
2020-02-11 20:34:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,9 +15,10 @@ class Scraper
 #      course.schedule = post.css(".date").text
 #      course.description = post.css("p").text
 #    end
-   
   end
   
+  def get_courses
+    self.get_page.css(".post")
   
   def print_courses
     self.make_courses
